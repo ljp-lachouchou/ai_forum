1fe8bdfc1823a10ae492bcdf4bb9548f201e53bc
aliyun8025300295-jM18Y <2950203059@qq.com>
2026-02-02 18:25:03 +0800

    新增navigation模块，提供路由导航作用

---
+++ core/navigation/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    alias(libs.plugins.aiforum.android.library)
+    alias(libs.plugins.aiforum.hilt)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.compose)
+}
+
+android {
+    namespace = "ai.ljp.navigation"
+
+}
+
+dependencies {
+
+    api(libs.androidx.navigation3.runtime)
+    implementation(libs.androidx.savedstate.compose)
+    implementation(libs.androidx.lifecycle.viewModel.navigation3)
+
+    testImplementation(libs.truth)
+
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+    androidTestImplementation(libs.androidx.test.ext)
+    androidTestImplementation(libs.androidx.compose.ui.testManifest)
+    androidTestImplementation(libs.androidx.lifecycle.viewModel.testing)
+    androidTestImplementation(libs.truth)
+}
