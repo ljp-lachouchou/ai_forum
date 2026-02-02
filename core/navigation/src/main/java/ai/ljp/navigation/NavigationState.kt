841dfafc9c20bb21f1eeda315c1a81f5965a4138
aliyun8025300295-jM18Y <2950203059@qq.com>
2026-02-02 19:08:37 +0800

    完成navigation模块

---
+++ core/navigation/src/main/java/ai/ljp/navigation/NavigationState.kt
@@ -0,0 +1,71 @@
+package ai.ljp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
+import androidx.navigation3.runtime.NavBackStack
+import androidx.navigation3.runtime.NavEntry
+import androidx.navigation3.runtime.NavKey
+import androidx.navigation3.runtime.rememberDecoratedNavEntries
+import androidx.navigation3.runtime.rememberNavBackStack
+import androidx.navigation3.runtime.rememberSaveableStateHolderNavEntryDecorator
+
+
+@Composable
+fun rememberNavigationState(
+    startKey: NavKey,
+    topKeys : Set<NavKey>
+) : NavigationState {
+    val topStack = rememberNavBackStack(startKey)
+    val subStacks = topKeys.associateWith { key -> rememberNavBackStack(key) }
+    return remember(startKey,topKeys) {
+        NavigationState(
+            startKey = startKey,
+            topStack = topStack,
+            subStacks = subStacks
+        )
+    }
+}
+
+class NavigationState(
+    val startKey : NavKey,
+    val topStack : NavBackStack<NavKey>,
+    val subStacks : Map<NavKey, NavBackStack<NavKey>>
+) {
+    //会被 @Composable 直接读取
+    val currentTopKey : NavKey by derivedStateOf { topStack.last() }
+
+    val topKeys
+        get() = subStacks.keys
+
+    val currentSubStack
+        get() = subStacks[currentTopKey] ?: error("$currentTopKey 子栈不存在")
+
+    //会被 @Composable 直接读取
+    val currentKey : NavKey by derivedStateOf { currentSubStack.last() }
+}
+@Composable
+fun NavigationState.toEntries(
+    entryProvider : (NavKey) -> NavEntry<NavKey>
+) : SnapshotStateList<NavEntry<NavKey>> {
+    val decoratorEntries =subStacks.mapValues { (_,stack) ->
+        val decorators = listOf(
+            rememberViewModelStoreNavEntryDecorator<NavKey>(),
+            rememberSaveableStateHolderNavEntryDecorator<NavKey>()
+        )
+        rememberDecoratedNavEntries(
+            entryProvider = entryProvider,
+            entryDecorators = decorators,
+            backStack = stack
+        )
+
+    }
+    return topStack
+        .flatMap {
+        decoratorEntries[it] ?: emptyList()
+    }.toMutableStateList()
+}
---
+++ core/navigation/src/main/java/ai/ljp/navigation/Navigator.kt
@@ -0,0 +1,47 @@
+package ai.ljp.navigation
+
+import androidx.navigation3.runtime.NavKey
+import kotlin.compareTo
+import kotlin.text.clear
+
+class Navigator(val state: NavigationState) {
+    fun navigate(key : NavKey) {
+        when(key) {
+            state.currentTopKey -> clearSubStack()
+            in state.topKeys -> goToTop(key)
+            else -> goToKey(key)
+        }
+    }
+
+    fun goBack() {
+        when(state.currentKey) {
+            state.startKey -> error("You cannot go back from the start route")
+            in state.topKeys -> error("You cannot go back from the top route")
+            else -> state.currentSubStack.removeLastOrNull()
+        }
+    }
+
+    private fun goToKey(key: NavKey) {
+        state.currentSubStack.apply {
+            remove(key)
+            add(key)
+        }
+    }
+
+    private fun goToTop(key: NavKey) {
+        state.topStack.apply {
+            if (key == state.startKey) {
+                clear()
+            }else {
+                remove(key)
+            }
+            add(key)
+        }
+    }
+
+    private fun clearSubStack() {
+        state.currentSubStack.run {
+            if (size > 1) subList(1, size).clear() // 只留下最底层的，也就是currentTopKey
+        }
+    }
+}
