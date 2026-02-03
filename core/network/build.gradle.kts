20d2b700a95dd0200902cf0d0e751524d588982e
aliyun8025300295-jM18Y <2950203059@qq.com>
2026-02-03 16:17:52 +0800

    创建network层，为ui提供数据源

---
+++ core/network/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    alias(libs.plugins.aiforum.android.library)
+    alias(libs.plugins.aiforum.hilt)
+    alias(libs.plugins.aiforum.ktor)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+android {
+    namespace = "ai.ljp.network"
+
+}
+
+dependencies {
+    implementation(libs.kotlinx.serialization.json)
+    api(libs.kotlinx.datetime)
+    api(projects.core.common)
+    api(projects.core.model)
+
+    implementation(libs.coil.kt)
+    implementation(libs.coil.kt.svg)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+}
--- core/simapi/src/main/java/io/ljp/simapi/example/MyApplication.kt
+++ core/simapi/src/main/java/io/ljp/simapi/example/MyApplication.kt
@@ -1,14 +1,8 @@
 package io.ljp.simapi.example
 
 import android.app.Application
-import io.ljp.simapi.ApiService
-import io.ljp.simapi.apiClient
-import io.ljp.simapi.apiRequest
 import io.ljp.simapi.module.LoggingModule
 
 class MyApplication : Application(){
-    override fun onCreate() {
-        super.onCreate()
-        ApiService.init(baseUrl = "https://www.csdn.net/", modules = listOf(LoggingModule(enable = true)))
-    }
+
 }
