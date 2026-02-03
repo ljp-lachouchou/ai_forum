611d53ef9ef476dfabb69dcd8bf3ab890b43aa63
aliyun8025300295-jM18Y <2950203059@qq.com>
2026-02-03 13:57:33 +0800

    编写搜索历史逻辑

--- core/database/src/main/java/ai/ljp/database/dao/RecentSearchQueryDao.kt
+++ core/database/src/main/java/ai/ljp/database/dao/RecentSearchQueryDao.kt
@@ -1,7 +1,20 @@
 package ai.ljp.database.dao
 
+import ai.ljp.database.model.RecentSearchQueryEntity
 import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface RecentSearchQueryDao {
+    @Query(value = "SELECT * FROM recentSearchQueries ORDER BY queriedDate DESC LIMIT :limit")
+    fun getRecentSearchQueryEntities(limit: Int): Flow<List<RecentSearchQueryEntity>>
+
+    @Upsert
+    suspend fun insertOrReplaceRecentSearchQuery(recentSearchQuery: RecentSearchQueryEntity)
+
+    @Query(value = "DELETE FROM recentSearchQueries")
+    suspend fun clearRecentSearchQueries()
+
 }
