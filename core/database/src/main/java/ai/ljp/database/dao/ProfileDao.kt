efbe66de5954b7d23565ed895a6488c697919fe0
aliyun8025300295-jM18Y <2950203059@qq.com>
2026-02-03 14:10:42 +0800

    完成ProfileDao

--- core/database/src/main/java/ai/ljp/database/dao/ProfileDao.kt
+++ core/database/src/main/java/ai/ljp/database/dao/ProfileDao.kt
@@ -1,7 +1,21 @@
 package ai.ljp.database.dao
 
+import ai.ljp.database.model.ProfileEntity
 import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ProfileDao {
+    @Upsert
+    suspend fun upsertProfile(profile : ProfileEntity)
+    @Query(
+        """
+            SELECT * 
+            FROM profiles 
+            WHERE `profileId` = :userId
+        """
+    )
+    fun getSelfProfile(userId : String) : Flow<ProfileEntity>
 }
