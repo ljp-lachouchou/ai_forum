e8346671e9baded1ab599d308b44fea29ede5221
aliyun8025300295-jM18Y <2950203059@qq.com>
2026-02-03 14:51:05 +0800

    完成NotificationDao

--- core/database/src/main/java/ai/ljp/database/dao/NotificationDao.kt
+++ core/database/src/main/java/ai/ljp/database/dao/NotificationDao.kt
@@ -1,7 +1,35 @@
 package ai.ljp.database.dao
 
+import ai.ljp.database.model.NotificationEntity
+import androidx.paging.PagingSource
 import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NotificationDao {
+    @Query("""
+    UPDATE notifications
+    SET `read` = 1
+    WHERE `notificationId` = :id
+""")
+    suspend fun markAsRead(id: String)
+
+    @Query("""
+    SELECT COUNT(*) FROM notifications
+    WHERE `read` = 0
+""")
+    fun unreadCount(): Flow<Int>
+
+    @Query("""
+        SELECT * FROM notifications
+        ORDER BY createdAt DESC
+    """)
+    fun getNotifications(): PagingSource<Int, NotificationEntity>
+
+    @Upsert
+    suspend fun upsertNotifications(
+        notifications: List<NotificationEntity>
+    )
 }
