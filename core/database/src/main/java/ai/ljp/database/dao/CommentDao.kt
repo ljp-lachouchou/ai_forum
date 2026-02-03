487797949e710821406490ed58c54dc8babf6bad
aliyun8025300295-jM18Y <2950203059@qq.com>
2026-02-03 14:57:15 +0800

    完成CommentDao

--- core/database/src/main/java/ai/ljp/database/dao/CommentDao.kt
+++ core/database/src/main/java/ai/ljp/database/dao/CommentDao.kt
@@ -1,7 +1,39 @@
 package ai.ljp.database.dao
 
+import ai.ljp.database.model.CommentEntity
+import androidx.paging.PagingSource
 import androidx.room.Dao
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CommentDao {
+    @Upsert
+    suspend fun insertAll(comments : List<CommentEntity>)
+
+    @Query(
+        """
+            SELECT * FROM comments 
+            WHERE `postId` = :postId
+        """
+    )
+    fun getCommentsByPostId (postId : String) : PagingSource<Int, CommentEntity>
+
+    @Query(
+        """
+            SELECT * FROM comments 
+            WHERE `commentId` = :commentId
+        """
+    )
+    fun getCommentById(commentId : String) : Flow<CommentEntity>
+
+    @Query(
+        """
+            DELETE FROM comments 
+            WHERE `commentId` = :commentId
+            
+        """
+    )
+    suspend fun deleteComment(commentId : String)
 }
