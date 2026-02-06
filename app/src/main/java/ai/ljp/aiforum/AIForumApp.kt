3151940c5a3b52cf07c5e5e45370f3710e6d978f
aliyun8025300295-jM18Y <2950203059@qq.com>
2026-02-06 21:10:35 +0800

    同步任务启用

--- app/src/main/java/ai/ljp/aiforum/AIForumApp.kt
+++ app/src/main/java/ai/ljp/aiforum/AIForumApp.kt
@@ -1,9 +1,12 @@
 package ai.ljp.aiforum
 
+import ai.ljp.sync.initializer.Sync
+import ai.ljp.sync.worker.SyncWorker
 import android.app.Application
 import cn.jpush.android.api.JPushInterface
 import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
 class AIForumApp : Application() {
@@ -12,5 +15,6 @@ class AIForumApp : Application() {
         FirebaseApp.initializeApp(this)
         JPushInterface.setDebugMode(true)
         JPushInterface.init(this)
+        Sync.initialize(this)
     }
 }
