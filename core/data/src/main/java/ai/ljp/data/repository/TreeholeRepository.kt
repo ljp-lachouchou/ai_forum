1efe2e4696ccd33e76ba23595ff59c39ecb281d3
aliyun8025300295-jM18Y <2950203059@qq.com>
2026-02-07 15:27:34 +0800

    修改TreeholeRepository接口

--- core/data/src/main/java/ai/ljp/data/repository/TreeholeRepository.kt
+++ core/data/src/main/java/ai/ljp/data/repository/TreeholeRepository.kt
@@ -6,9 +6,10 @@ import com.ljp.model.Treehole
 import kotlinx.coroutines.flow.Flow
 
 interface TreeholeRepository : Syncable {
-    suspend fun upsertTreeholes(treeholes : List<Treehole>)
-
-    suspend fun insertTreehole(treehole : Treehole)
+    suspend fun createTreehole(
+        content: String,
+        isAnonymous: Boolean
+    )
 
     fun getTreeholes() : Flow<PagingData<Treehole>>
 }
