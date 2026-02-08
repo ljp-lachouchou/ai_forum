6fcba050d35f72c4908a653f6226c7899ae5e5a2
aliyun8025300295-jM18Y <2950203059@qq.com>
2026-02-08 13:43:19 +0800

    新增ui模块，为ui层提供可抽象出来的ui复合组件

---
+++ core/ui/build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    alias(libs.plugins.aiforum.android.library)
+    alias(libs.plugins.aiforum.android.library.compose)
+}
+
+android {
+    namespace = "ai.ljp.ui"
+}
+
+dependencies {
+
+    api(libs.androidx.metrics)
+    api(projects.core.analytics)
+    api(projects.core.designsystem)
+    api(projects.core.model)
+
+    implementation(libs.coil.kt)
+    implementation(libs.coil.kt.compose)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+}
