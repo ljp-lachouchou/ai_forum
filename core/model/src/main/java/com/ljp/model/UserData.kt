55b667090c33626fc15cb988279d360440ccbac1
aliyun8025300295-jM18Y <2950203059@qq.com>
2026-02-12 23:27:56 +0800

    创建Treehole的api库

--- core/model/src/main/java/com/ljp/model/UserData.kt
+++ core/model/src/main/java/com/ljp/model/UserData.kt
@@ -9,7 +9,7 @@ data class UserData(
     val useDynamicColor: Boolean,
     val shouldHideOnboarding: Boolean, // 打开时，是否需要选择一些东西，作为画像进行文章推荐
     val currentUserId: String?,
-    val authToken: String,
+    val authToken: String?,
     val lastSyncVersion: Long = 0L,
     val moodThemeConfig : MoodThemeConfig
 
