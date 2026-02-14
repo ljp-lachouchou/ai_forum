42e8ba7a14266c1c49981e6beb30096c301777cd
aliyun8025300295-jM18Y <2950203059@qq.com>
2026-02-14 19:38:10 +0800

    创建PostCreate:impl库

---
+++ Feature/PostCreate/impl/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    alias(libs.plugins.aiforum.android.feature.impl)
+    alias(libs.plugins.aiforum.android.library.compose)
+}
+
+android {
+    namespace = "feature.ljp.postcreate.impl"
+
+}
+
+dependencies {
+
+    implementation(projects.core.domain)//
+    implementation(projects.feature.postCreate.api)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+}
