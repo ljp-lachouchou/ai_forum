de37070972530ae9d08fc9b1164e4086aac5a11e
aliyun8025300295-jM18Y <2950203059@qq.com>
2026-02-14 12:33:02 +0800

    新增search:impl库，创建SearchScreen

---
+++ Feature/Search/api/src/main/java/feature/ljp/search/api/SearchNavKey.kt
@@ -0,0 +1,7 @@
+package feature.ljp.search.api
+
+import androidx.navigation3.runtime.NavKey
+import kotlinx.serialization.Serializable
+
+@Serializable
+object SearchNavKey : NavKey
---
+++ Feature/Search/impl/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    alias(libs.plugins.aiforum.android.feature.impl)
+    alias(libs.plugins.aiforum.android.library.compose)
+}
+
+android {
+    namespace = "feature.ljp.search.impl"
+
+}
+
+dependencies {
+
+    implementation(projects.core.domain)//
+    implementation(projects.feature.search.api)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.test.espresso.core)
+}
